1b934234bee632b6cf5a9615c44c120b88783992
Igor Bykov <igorb.dev@websm.io>
2018-10-22 15:22:41 +0300

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,22 @@ use libjail::*;
 use std::net::{Ipv4Addr, Ipv6Addr};
 
 fn main() {
-    let _rules: HashMap<Val, Val> = HashMap::new();
 
-    // rules.insert("path".into(), "/jails/freebsd112".into());
-    // rules.insert("name".into(), "freebsd112".into());
-    // rules.insert("ip4.addr".into(), "127.0.0.1".parse::<Ipv4Addr>().unwrap().into());
-    // rules.insert("ip6.addr".into(), "::123".parse::<Ipv6Addr>().unwrap().into());
+    let mut rules: HashMap<Val, Val> = HashMap::new();
+
+    rules.insert("jid".into(), 2.into());
+    rules.insert("path".into(), "/jails/freebsd112".into());
+    rules.insert("name".into(), "freebsd112".into());
+    rules.insert("ip4.addr".into(), "127.0.0.1".parse::<Ipv4Addr>().unwrap().into());
     // rules.insert("persist".into(), true.into());
-    // rules.insert("nopersist".into(), Val::Null);
+    rules.insert("nopersist".into(), Val::Null);
 
-    // let jid = set(rules, Action::create() + Modifier::attach()).unwrap();
+    let jid = set(rules, Action::create() + Modifier::attach()).unwrap();
 
-    let rules = get_rules(1, vec!["name", "ip4.addr", "jid", "ip6.addr"]).unwrap();
-    println!("-- {:?}", rules);
+    loop {}
+
+    // let rules = get_rules(1, vec!["name", "ip4.addr", "jid", "ip6.addr"]).unwrap();
+    // println!("-- {:?}", rules);
 
     // rules.insert("jid".into(), 1.into());
     // // rules.insert("name".into(), "freebsd112".into());
